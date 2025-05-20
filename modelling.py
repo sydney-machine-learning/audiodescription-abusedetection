231ecf8bb787126083dc0408e252e92252898325
Eye4got <bianra16@gmail.com>
2025-05-20 20:44:10 +1000

    Fix non-english segments and rerun transcription

--- modelling.py
+++ modelling.py
@@ -2,3 +2,17 @@ import pandas as pd
 import numpy as np
 
 import torch
+
+class AudioSegDataset(torch.utils.data.Dataset):
+    
+    def __init__(self, encodings, labels):
+        self.encodings = {key: torch.tensor(val) for key, val in encodings.items()}
+        self.labels = torch.tensor(labels.values.astype('int').reshape(-1, 1), dtype=torch.float32)
+        
+    def __getitem__(self, idx):
+        item = {key: val[idx] for key, val in self.encodings.items()}
+        item['labels'] = self.labels[idx]
+        return item
+    
+    def __len__(self):
+        return len(next(iter(self.encodings.values())))
