ee41dcb93f20d88d85cce772e8acf7d251b50397
Eye4got <bianra16@gmail.com>
2025-05-22 15:04:37 +1000

    Tokenization refactor

--- data_extraction.py
+++ data_extraction.py
@@ -1,9 +1,14 @@
 import pandas as pd
+import numpy as np
 
 import os
 import logging
 import re
 from typing import List, Tuple
+
+from transformers import (
+    AutoTokenizer
+)
 
 import utils
 
@@ -28,6 +33,7 @@ def convert_time_to_readable_txt(df: pd.DataFrame, cols: List[Tuple[str, str]]):
 
 
 def extract_subs():
+    
     subs_file_list = [os.path.join(sub_dir, x) for x in os.listdir(sub_dir) if not x.endswith('.zip')]
     movie_subs_dict = {}
 
@@ -137,3 +143,124 @@ def extract_vsd_annotations_file(path):
         df_rows.append(row)
         
     return pd.DataFrame(df_rows)
+
+
+def aggregate_segments(df: pd.DataFrame, tokenizer, enc_len: int, max_seg_duration: int, max_break: int, excl_dialogue: bool):
+    
+    df['num_tokens'] = [len(x) for x in tokenizer(list(df['text']))['input_ids']]
+    df = df.sort_values(['movie', 'start'])
+    
+    agg_rows_list = []
+    prev_row = dict(df.iloc[0])
+
+    for row in df.iloc[1:].to_dict(orient="records"):
+        
+        is_under_token_limit = (prev_row['num_tokens'] + row['num_tokens']) < enc_len
+        is_under_duration_limit = (row['end'] - prev_row['start']) < max_seg_duration
+        is_under_max_break = (row['start'] - prev_row['end']) < max_break
+        is_same_speaker = prev_row['movie'] == row['movie'] and prev_row['speaker'] == row['speaker'] 
+        
+        if all((is_same_speaker, is_under_max_break, is_under_duration_limit, is_under_token_limit)):
+            prev_row['end_txt'] = row['end_txt']
+            prev_row['duration'] += row['duration']
+            prev_row['text'] += (' ' + row['text'])
+            prev_row['end'] = row['end']
+            prev_row['num_tokens'] += row['num_tokens']
+            
+        else:
+            agg_rows_list.append(prev_row)
+            prev_row = row.copy()
+        
+    agg_seg_df = pd.DataFrame.from_records(agg_rows_list).sort_values(['movie', 'start']).reset_index(drop=True)
+
+    if excl_dialogue:
+        agg_seg_df = agg_seg_df[agg_seg_df.is_dialogue.eq(False)].reset_index(drop=True)
+        
+    return agg_seg_df
+
+
+def annotate_segments(agg_seg_df: pd.DataFrame, gore_df: pd.DataFrame, tokenizer):
+    
+    # Mark each segment as containing gore by looping through all the gore annotations
+    agg_seg_df['has_gore'] = False
+    agg_seg_df['has_blood'] = False
+    agg_seg_df['blood_cat'] = pd.NA
+
+    agg_seg_df['gore_start'] = -1 
+    agg_seg_df['gore_end'] = -1 
+
+    agg_seg_df['blood_start'] = -1 
+    agg_seg_df['blood_end'] = -1 
+
+    seg_df_list = []
+
+    for movie in gore_df.movie.unique():
+        
+        curr_gore_df = gore_df[gore_df.movie.eq(movie)].copy()
+        curr_seg_df = agg_seg_df[agg_seg_df.movie.eq(movie)].copy()
+        
+        for ii in range(curr_gore_df.shape[0]):
+            
+            cat = 'gore' if 'gore' in curr_gore_df['annotation_cat'].iloc[ii] else 'blood'
+            
+            # Identify any segments that overlap with gore segment
+            anot_start_after_seg = curr_seg_df.start > curr_gore_df['end_sec'].iloc[ii]
+            seg_end_before_anot = curr_seg_df.end < curr_gore_df['start_sec'].iloc[ii]
+            curr_anot_mask = np.logical_not(anot_start_after_seg | seg_end_before_anot)
+            
+            if sum(curr_anot_mask) > 0:
+                curr_seg_df[f'{cat}_start'] = np.where(curr_anot_mask, curr_gore_df['start_sec'].iloc[ii], curr_seg_df[f'{cat}_start'])
+                curr_seg_df[f'{cat}_end'] = np.where(curr_anot_mask, curr_gore_df['end_sec'].iloc[ii], curr_seg_df[f'{cat}_end'])
+                
+                curr_seg_df[f'has_{cat}'] = curr_seg_df[f'has_{cat}'] | curr_anot_mask
+                
+                if cat == 'blood':
+                    curr_seg_df['blood_cat'] = np.where(curr_anot_mask, curr_gore_df['full_annotation_cat'].iloc[ii], curr_seg_df['blood_cat'])
+                    
+            # No overlap, so find closest segment   
+            else:
+                annot_midpoint = (curr_gore_df['start_sec'].iloc[ii] + curr_gore_df['end_sec'].iloc[ii]) / 2
+                closest_start_idx = next(iter(curr_seg_df.start[curr_seg_df.start > annot_midpoint].index), None)
+                closest_start = abs(curr_seg_df.start[closest_start_idx] - annot_midpoint) if closest_start_idx else curr_seg_df.end.iloc[-1]
+                closest_end_idx = max(0, closest_start_idx - 1) if closest_start_idx else curr_seg_df.index[-1]
+                closest_end = abs(curr_seg_df.end[closest_end_idx] - annot_midpoint) if closest_end_idx else curr_seg_df.end.iloc[-1]
+                
+                closest_seg_idx = closest_end_idx
+                
+                if closest_start is not None and closest_end is not None and closest_start < closest_end:
+                    closest_seg_idx = closest_start_idx
+                    
+                curr_seg_df.loc[closest_seg_idx, f'{cat}_start'] = curr_gore_df['start_sec'].iloc[ii].astype(int)
+                curr_seg_df.loc[closest_seg_idx, f'{cat}_end'] = curr_gore_df['end_sec'].iloc[ii].astype(int)
+                
+                curr_seg_df.loc[closest_seg_idx, f'has_{cat}'] = True
+                
+                if cat == 'blood':
+                    curr_seg_df.loc[closest_seg_idx, 'blood_cat'] = curr_gore_df['full_annotation_cat'].iloc[ii]
+                    
+        seg_df_list.append(curr_seg_df)
+        
+    cat_seg_df = pd.concat(seg_df_list).reset_index(drop=True)
+    
+    cat_seg_df['cat'] = np.select(
+        [cat_seg_df['has_gore'] | cat_seg_df['blood_cat'].isin(['blood-high', 'blood-medium']), cat_seg_df.has_blood],
+        ['gore', 'blood'],
+        default='neutral'
+    )
+    
+    cat_seg_df['tokens'] = tokenizer(list(cat_seg_df['text']), add_special_tokens=False)['input_ids']
+
+    return cat_seg_df
+
+
+def add_context_and_tokenize(cat_seg_df: pd.DataFrame, tokenizer, enc_len: int):
+    
+    tokens_w_context = []
+
+    for ii in range(cat_seg_df.shape[0]):
+        context_len = (enc_len - cat_seg_df['num_tokens'][ii]) // 2
+        prev_seg = tokenizer.decode(cat_seg_df['tokens'][ii - 1][-context_len:]) if ii > 0 else ''
+        next_seg = tokenizer.decode(cat_seg_df['tokens'][ii + 1][:context_len]) if ii < cat_seg_df.shape[0] - 1 else ''
+        tokens_w_context.append('[CLS]' + prev_seg + '[SEP]' + tokenizer.decode(cat_seg_df['tokens'][ii]) + '[SEP]' + next_seg)
+        
+    return tokens_w_context
--- modelling.py
+++ modelling.py
@@ -62,15 +62,22 @@ class FractionalTrainEvalCallback(TrainerCallback):
 class CustomLossModel(torch.nn.Module):
     def __init__(self, model_name: str, loss_fn: Callable, unfrozen_layers: List[int]):
         super().__init__()
-        self.model = AutoModelForSequenceClassification.from_pretrained(model_name, num_labels=1)#, torch_dtype=torch.bfloat16, attn_implementation="flash_attention_2").to('cuda')
+        self.model = AutoModelForSequenceClassification.from_pretrained(
+            model_name,
+            num_labels=1
+            # classifier_dropout=0.3
+        )
+        
+        #, torch_dtype=torch.bfloat16, attn_implementation="flash_attention_2").to('cuda')
         self.loss_fn = loss_fn
         
         # Freeze early layers
         
         for name, param in self.model.named_parameters():
-            # Match patterns like encoder.layer.X or encoder.layers.X (ModernBERT might use layers)
+            # Match patterns like encoder.layer.X or encoder.layers.X
+            lm_head_names = ('classifier', 'head', 'final', 'pooler')
             match = re.search(r'\.(?:layer|layers)\.(\d+)\.', name)
-            if "classifier" in name or "head" in name or "final" in name:
+            if any(x in name for x in lm_head_names):
                 param.requires_grad = True
             elif match:
                 layer_num = int(match.group(1))
@@ -98,31 +105,38 @@ class CustomLossModel(torch.nn.Module):
 
 
 def compute_metrics(eval_pred):
-    logits, labels = eval_pred
-    preds = np.argmax(logits, axis=1)
+    logits, labels = eval_pred  # logits shape: (N, 1), labels shape: (N,)
     
-    # Macro & binary F1/precision/recall
+    # Convert logits to probabilities using sigmoid
+    probs = 1 / (1 + np.exp(-logits))
+    preds = (probs >= 0.5).astype(int).reshape(-1)
+    labels = labels.reshape(-1)
     precision, recall, f1, _ = precision_recall_fscore_support(labels, preds, average='binary', zero_division=0)
-
-    # auroc = roc_auc_score(labels, logits[:, 1])
     has_gore_mask = labels == 1
 
     return {
         "accuracy": accuracy_score(labels, preds),
-        "gore_accuracy": accuracy_score(labels[has_gore_mask], preds[has_gore_mask.reshape(-1)]),
+        "gore_accuracy": accuracy_score(labels[has_gore_mask], preds[has_gore_mask]),
         "precision": precision,
         "recall": recall,
         "f1": f1,
         "f1_macro": f1_score(labels, preds, average='macro')
-        # "auroc": auroc
     }
     
 
-def create_test_train_split(df: pd.DataFrame, tokenized_input, movie_list: List[str], tokenizer):
+def create_test_train_split(df: pd.DataFrame, tokenized_input, movie_list: List[str], tokenizer, enc_len):
     set_mask = df['movie'].isin(movie_list)
-    set_enc = tokenizer([txt for in_set, txt in zip(set_mask, tokenized_input) if in_set], add_special_tokens=False, padding=True)
+    set_enc = tokenizer([txt for in_set, txt in zip(set_mask, tokenized_input) if in_set], add_special_tokens=False, padding='max_length', max_length=enc_len)
     set_y = df['has_gore'][set_mask]
 
     ds = AudioSegDataset(set_enc, set_y)
     
     return ds, set_y 
+
+
+def check_trainable_layers(model):
+    for name, param in model.named_parameters():
+        if not param.requires_grad:
+            print(f"[FROZEN] {name}")
+        else:
+            print(f"[TRAINABLE] {name}")
